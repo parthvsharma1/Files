2276a8d2c548ec822ea5102daad9dc772b833aeb
parthvsharma1 <parth.sharma1@sprinklr.com>
2022-06-11 01:13:23 +0530

    adding classes

---
+++ src/main/java/com/spr/shapes/Circle.java
@@ -0,0 +1,16 @@
+package com.spr.shapes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Circle {
+    Quadrilateral quadrilateral;
+    @Autowired
+    public Circle( Quadrilateral quadrilateral){
+        this.quadrilateral=quadrilateral;
+        System.out.println("MADE A CIRCLE OBJECT !!!!!!!!");
+    }
+
+}
---
+++ src/main/java/com/spr/shapes/Square.java
@@ -0,0 +1,14 @@
+package com.spr.shapes;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class Square implements Quadrilateral {
+    @Override
+    public String getShape(){
+        return "square";
+    }
+}
